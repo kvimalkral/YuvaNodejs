a4f49a5e5239ca7d55701f4895fd6e512deba2ac
unknown <kvimalkral@gmail.com>
2021-07-27 21:19:12 +0530

    first

---
+++ server.js
@@ -0,0 +1,92 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+var cors = require('cors')
+const app = express();
+app.use(cors());
+
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+mongoose
+  .connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((sucess) => {
+    console.log("connected sucussfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+const customer = mongoose.model("customer", {
+  salutation: {type:String},
+  customerName: { type: String, required: true },
+  phone: {type:Number, required: true, unique: true},
+  email: {type:String, unique: true, required: true},
+  address: { type: String},
+  pinCode: {type: Number},
+  age: {type: Number},
+  vistedOn: {type: Date}
+});
+app.post("/users", (req, res) => {
+    customer.create(req.body).
+    then((success) => {
+        res.json(success);
+    })
+    .catch( (error) => {
+         res.send(error)
+    })
+    
+});
+
+app.get("/users", (req, res) => {
+    customer.find({}).
+    then((success) => {
+        res.json(success);
+    })
+    .catch( (error) => {
+         res.send(error)
+    })
+   
+});
+app.get("/users/:id", (req, res) => {
+  customer.findOne({_id: req.params.id}).
+  then((success) => {
+      res.json(success);
+  })
+  .catch( (error) => {
+       res.send(error)
+  })
+ 
+});
+
+
+
+app.put("/users/:id", (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    customer.updateOne({_id: req.params.id }, {$set: req.body}).
+    then((success) => {
+        res.json(success);
+    })
+    .catch( (error) => {
+         res.send(error)
+    })
+   
+});
+
+app.delete("/users/:userName", (req,res) => {
+  customer.deleteOne({_id: req.params.userName }).
+    then((success) => {
+        res.json(success);
+    })
+    .catch( (error) => {
+         res.send(error)
+    })
+})
+
+
+
+app.listen(4000, () => {
+  console.log("app listing to port 4000");
+});
